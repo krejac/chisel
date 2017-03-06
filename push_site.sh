44bd5085861e3267888ff1d526c8d8ed1f630be3
Kresten Jacobsen <kresten@logiskhave.dk>
2017-03-06 09:42:37 +0100

    Added autocomit of posts/ when publishing (+ output formatting)

--- push_site.sh
+++ push_site.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 echo ""
+echo "Comitting raw post(s) to git (master-branch)"
+echo ""
+git add ./posts/
+git commit -m "Version auto-comitted"
+git push
+
+echo ""
+echo "**************************"
+echo ""
 echo "(Re-)gererating site ..."
 echo ""
 python chisel.py
 
+echo ""
+echo "**************************"
 echo ""
 echo "Updating gh-pages ..."
 echo ""
