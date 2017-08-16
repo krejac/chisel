dbc74a329372008461fd7cf6a73c22d7a830b63d
Kresten Jacobsen <kresten@logiskhave.dk>
2017-08-16 15:46:49 +0200

    Virtualenv functionality added

--- push_site.sh
+++ push_site.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo ""
+echo "Activating Python environment"
+echo ""
+source ./venv/bin/activate
+
+echo ""
+echo "**************************"
+echo ""
 echo "Comitting raw post(s) to git (master-branch)"
 echo ""
 git add ./posts/
@@ -20,3 +27,10 @@ echo ""
 echo "Updating gh-pages ..."
 echo ""
 ghp-import -n -c "log.logiskhave.dk" -m "Site updated :rocket:" -p ./www/
+
+echo ""
+echo "**************************"
+echo ""
+echo "Deactivating Python environment"
+echo ""
+deactivate
